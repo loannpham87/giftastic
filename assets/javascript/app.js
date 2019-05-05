4638df4226a85b2bfbaef141949b16a4802b0c2a
loannpham87 <loannpham87@gmail.com>
2019-05-04 18:57:47 -0700

    final push, did not get add button function working

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,38 +1,72 @@
-//function for tastic swoop on window load
-function tastic() {
-    $("#tastic").animate({
-        right: '900px',
-        height: '100px',
-        width: '150px'
-    });
-};
+$(document).ready(function () {
 
-// array of buttons populate at the top of the page for specific searches, need a loop, when a button is clicked, that is when the 10 gifs will populate
-
-//event listener for buttons
-$("button").on("click", function () {
-    var thankunext = $(this).attr("data-thank-u-next");
-
-    var queryURL = "https://api.giphy.com/v1/gifs/search?api_key=caUXL8fwGwuoChfEtrkOWsYnE6mAsbfm&q=thank u, next&limit=10&offset=0&rating=G&lang=en";
-
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-})
-
-    .then(function (response) {
-        var results = response.data;
-
-        for (var i = 0; i < results.length; i++) {
-            if (results[i].rating !== "r" && results[i] !== "pg-13") {
-                var popGifs = $("#pop-gifs");
+    //function for existing buttons
+    $(document).on("click", ".existing", function () {
+        $("#pop-gifs").empty()
+        var type = $(this).attr("data-type");
+        var queryURL = "http://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=caUXL8fwGwuoChfEtrkOWsYnE6mAsbfm&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div id='pop-gifs'>");
                 var rating = results[i].rating;
-                var ratingTag = $("#rating").text("Rating: " + rating);
-                popGifs.append(ratingTag);
-                $("pop-gifs").prepend(popGifs);
+                var popRatings = $("<p class='rating'>").text("Rating: " + rating);
+                var gifs = $("<img>")
+                var animate = results[i].images.fixed_height.url;
+                var still = results[i].images.fixed_height_still.url;
+                gifs.attr("src", still);
+                gifs.attr("data-still", still);
+                gifs.attr("data-animate", animate);
+                gifs.attr("data-state", "still");
+                gifs.addClass("existing");
+                gifDiv.append(popRatings);
+                gifDiv.append(gifs);
+                $("#pop-gifs").prepend(gifDiv);
             }
-        }
+        });
     });
 
-    
+    // function to add user's buttons
+    function newButton() {
+
+        var userAdd = $("<button>");
+        userAdd.addClass("existing");
+        userAdd.attr("data-type");
+        userAdd.text("user button"); //how to add text to the button using the user input
+        $(".buttons-section").append(userAdd);
+
+    }
+
+    $(".add-button").on("click", function () {
+        newButton();
+        $("#pop-gifs").empty()
+        var type = $(".user-button").attr("data-type");
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + type + "&api_key=qXf9cVvLHIlxjF4i7zNFUpH1uUFAj8Dl&limit=10";
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function (response) {
+            var results = response.data
+            for (var i = 0; i < results.length; i++) {
+                var gifDiv = $("<div id='pop-gifs'>");
+                var rating = results[i].rating;
+                var popRatings = $("<p class='rating'>").text("Rating: " + rating);
+                var animate = results[i].images.fixed_height.url;
+                var still = results[i].images.fixed_height_still.url;
+                var gifs = $("<img>")
+                gifs.attr("src", still);
+                gifs.attr("data-still", still);
+                gifs.attr("data-animate", animate);
+                gifs.attr("data-state", "still");
+                gifs.addClass("existing");
+                gifDiv.append(popRatings);
+                gifDiv.append(gifs);
+                $("#pop-gifs").prepend(gifDiv);
+            }
+        });
+    });
+
+});
